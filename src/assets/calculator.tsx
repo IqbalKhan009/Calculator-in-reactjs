66c53c796ead42622e79832cdb7f485b469efa16
IqbalKhan009 <74900949+IqbalKhan009@users.noreply.github.com>
2024-03-16 03:33:49 +0500

    Added Functionality to buttons

--- src/assets/calculator.tsx
+++ src/assets/calculator.tsx
@@ -1,33 +1,58 @@
+import { useState } from "react";
+
 function Calculator() {
+  const [display, setdisplay] = useState("");
+
+  const handleClick = (value: any) => {
+    setdisplay(display + value);
+  };
+
+  const CalcResult = () => {
+    try {
+      const result = eval(display);
+      setdisplay(result.toString());
+    } catch (error) {
+      setdisplay("Error");
+    }
+  };
+  const cleardisplay = () => {
+    setdisplay("");
+  };
   return (
     <>
       {" "}
       <div>
-        <input type="number" />
+        <input type="text" value={display} />
       </div>
       <div>
-        <button>1</button>
-        <button>2</button>
-        <button>3</button>
+        <button onClick={() => handleClick("1")}>1</button>
+        <button onClick={() => handleClick("2")}>2</button>
+        <button onClick={() => handleClick("3")}>3</button>
       </div>
       <div>
-        <button>4</button>
-        <button>5</button>
-        <button>6</button>
+        <button onClick={() => handleClick("4")}>4</button>
+        <button onClick={() => handleClick("5")}>5</button>
+        <button onClick={() => handleClick("6")}>6</button>
       </div>
       <div>
-        <button>7</button>
-        <button>8</button>
-        <button>9</button>
+        <button onClick={() => handleClick("7")}>7</button>
+        <button onClick={() => handleClick("8")}>8</button>
+        <button onClick={() => handleClick("9")}>9</button>
       </div>
       <div>
-        <button>+</button>
-        <button>-</button>
-        <button>/</button>
+        <button onClick={() => handleClick("+")}>+</button>
+        <button onClick={() => handleClick("-")}>-</button>
+        <button onClick={() => handleClick("/")}>/</button>
       </div>
       <div>
-        <button>*</button>
-        <button>=</button>
+        <button onClick={() => handleClick("*")}>*</button>
+        <button onClick={() => CalcResult()}>=</button>
+        <button onClick={() => cleardisplay()}>C</button>
+      </div>
+      <div>
+        <button onClick={() => handleClick("0")}>0</button>
+        <button onClick={() => handleClick(".")}>.</button>
+        <button onClick={() => handleClick("%")}>%</button>
       </div>
     </>
   );
